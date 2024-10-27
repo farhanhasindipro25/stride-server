7f824486f9eff08e16c62527a989457a5ad426d2
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-10-28 01:57:38 +0600

    feat: Added API endpoints for fetching tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Res } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './tasks.dto';
 import { Response } from 'express';
@@ -11,6 +11,14 @@ export class TasksController {
   @Post('/create')
   async createCategory(@Body() data: CreateTaskDto, @Res() res: Response){
     const result: Result = await this.tasksService.createTask(data);
+
+    return res.status(result.status).json(result)
+  }
+
+
+  @Get()
+  async getTasks(@Res() res: Response){
+    const result: Result = await this.tasksService.getTasks()
 
     return res.status(result.status).json(result)
   }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -9,7 +9,6 @@ export class TasksService {
   constructor(private readonly prisma: PrismaService) { }
 
   async createTask(data: CreateTaskDto): Promise<Result> {
-
     try {
       const task = await this.prisma.tasks.create({
         data: {
@@ -39,6 +38,24 @@ export class TasksService {
         error: error.message
       }
     }
+  }
 
+  async getTasks(): Promise<Result> {
+    try {
+      const tasks = await this.prisma.tasks.findMany()
+      return {
+        status: 200,
+        message: "Tasks fetched successfully",
+        context:'TasksService - getTasks',
+        data: tasks
+      }
+    } catch (error) {
+      return {
+        status: 500,
+        message: "Internal Server Error",
+        context:'TasksService - getTasks',
+        error: error.message
+      }
+    }
   }
 }
