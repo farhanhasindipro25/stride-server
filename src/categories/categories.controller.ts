ba361b03d25d1ca7f880a1d4051d7d75a40ddfbe
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-10-27 23:16:20 +0600

    refactor: Added try catch block in categories service

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,15 +1,19 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Res } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
 import { CreateCategoryDto } from './categories.dto';
 import { Categories } from '@prisma/client';
+import { Response } from 'express';
+import { Result } from 'src/_libs/interfaces/api-result.interface';
 
 @Controller('categories')
 export class CategoriesController {
   constructor(private categoriesService: CategoriesService) {}
 
   @Post('/create')
-  async createCategory(@Body() data: CreateCategoryDto): Promise<Categories>{
-    return this.categoriesService.createCategory(data);
+  async createCategory(@Body() data: CreateCategoryDto, @Res() res: Response){
+    const result: Result = await this.categoriesService.createCategory(data);
+
+    return res.status(result.status).json(result)
   }
 
   @Get()
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -4,27 +4,53 @@ import generateSlug from 'src/_libs/utils/slugGenerator';
 import generateUID from 'src/_libs/utils/uidGenerators';
 import { PrismaService } from 'src/shared/prisma/prisma.service';
 import { CreateCategoryDto } from './categories.dto';
+import { Result } from 'src/_libs/interfaces/api-result.interface';
 
 @Injectable()
 export class CategoriesService {
-  constructor(private prisma: PrismaService){}
+  constructor(private prisma: PrismaService) { }
 
 
-  async createCategory(data: CreateCategoryDto): Promise<Categories>{
-    const uid = generateUID(6)
-    const slug = generateSlug(data.name)
-    const categoryData = {
-      ...data,
-      uid: uid,
-      slug: slug,
-    };
+  async createCategory(data: CreateCategoryDto): Promise<Result> {
+    try {
+      const uid = generateUID(6)
+      const slug = generateSlug(data.name)
 
-    return this.prisma.categories.create({
-      data: categoryData,
-    });
+      const category = await this.prisma.categories.create({
+        data: {
+          ...data,
+          uid: uid,
+          slug: slug,
+        },
+      });
+      return {
+        status: 201,
+        message: "Category created successfully",
+        data: category,
+      }
+    } catch (error) {
+      return {
+        status: 500,
+        message: "Internal Server Error",
+        error: error.message
+      }
+    }
   }
 
-  async getCategories(): Promise<Categories[]>{
-    return this.prisma.categories.findMany()
+  async getCategories(): Promise<Result> {
+    try {
+      const categories = await this.prisma.categories.findMany()
+      return{
+        status: 200,
+        message:"Categories fetched successfully",
+        data: categories
+      }
+    } catch (error) {
+      return {
+        status: 500,
+        message: "Internal Server Error",
+        error: error.message
+      }
+    }
   }
 }
