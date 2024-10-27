802c97fbc49f26f8aae41f186bb648749be66609
farhanhasindipro25 <farhan.hasin.25@gmail.com>
2024-10-28 01:05:54 +0600

    feat: Added API endpoint for creating tags

--- src/tags/tags.controller.ts
+++ src/tags/tags.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+
+import { Body, Controller, Delete, Get, Param, Patch, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
+import { Result } from 'src/_libs/interfaces/api-result.interface';
+import { CreateTagDto, UpdateTagDto } from './tags.dto';
+import { TagsService } from './tags.service';
 
 @Controller('tags')
-export class TagsController {}
+export class TagsController {
+  constructor(private tagsService: TagsService) {}
+
+  @Post('/create')
+  async createTag(@Body() data: CreateTagDto, @Res() res: Response){
+    const result: Result = await this.tagsService.createTag(data);
+
+    return res.status(result.status).json(result)
+  }
+}
